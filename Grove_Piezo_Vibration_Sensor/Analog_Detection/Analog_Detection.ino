f2a5505a1322c63db90e2cf048b0f85116ff0a70
U-ENT\a0224668 <a0224668@CNA0224668.am.dhcp.ti.com>
2015-06-16 12:45:35 -0500

    Added cc3200 support for LED bar, added Piezo Vibration Sensor

---
+++ Grove_Piezo_Vibration_Sensor/Analog_Detection/Analog_Detection.ino
@@ -0,0 +1,43 @@
+/*
+Grove Piezo Vibration Sensor - Analog Detection Example
+http://www.seeedstudio.com/wiki/Grove-_Piezo_Vibration_Sensor
+
+Plug the Vibration sensor into port J17(D). Use the potentiometer to adjust sensitivity
+CW - Decrease :: CCW - Increase
+
+Ported for MSP-EXP430F5529, MSP-EXP432P401R, TM4c123 (Tiva C), and cc3200 LaunchPads By Cameron P. LaFollette
+
+ ***IMPORTANT*** If using cc3200 LaunchPad, place a jumper wire from 
+ BoosterPack pin 2 to 27
+ then change int sensorPin = 27 to 9
+ Take note, that due to the nature of cc3200's analog pins,the potentiometer will need to be trimmed 
+ very far CW, but not completely
+ 
+ */
+ 
+
+
+const int ledPin=RED_LED;
+const int sensorPin=27;// for cc3200 cange to const int sensorPin=2;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
+  //pinMode(sensorPin,INPUT);
+
+} 
+ 
+void loop() {
+int sensorValue = analogRead(sensorPin); 
+ Serial.println(sensorValue);
+ delay(50);
+ if(sensorValue>=1023)//needs to be 800 for msp432, 
+  {
+    digitalWrite(ledPin,HIGH);
+  }
+ else
+ {
+  digitalWrite(ledPin,LOW);
+  }
+}
+
---
+++ Grove_Piezo_Vibration_Sensor/Digital_Detection/Digital_Detection.ino
@@ -0,0 +1,39 @@
+/*
+Grove Piezo Vibration Sensor - Digital Detection Example
+http://www.seeedstudio.com/wiki/Grove-_Piezo_Vibration_Sensor
+
+Plug the Vibration sensor into port J17(D). Use the potentiometer to adjust sensitivity
+CW - Decrease :: CCW - Increase
+
+Ported for MSP-EXP430F5529, MSP-EXP432P401R, TM4c123 (Tiva C), and cc3200 LaunchPads By Cameron P. LaFollette
+
+ ***IMPORTANT*** If using cc3200 LaunchPad, place a jumper wire from 
+ BoosterPack pin 31 to 36
+ then change sensorPin from 36 to 31
+ Take note, that due to the nature of cc3200's analog pins,the potentiometer will need to be trimmed 
+ very far CW, but not completely
+ 
+ */
+
+const int ledPin=RED_LED;
+const int sensorPin=36; //for cc3200 change to const int sensorPin=31; 
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(ledPin,OUTPUT);
+  pinMode(sensorPin, INPUT);
+} 
+ 
+void loop() {
+int sensorState = digitalRead(sensorPin); 
+ Serial.println(sensorState);
+ delay(50);
+ if(sensorState == HIGH)
+  {
+    digitalWrite(ledPin,HIGH);
+  }
+ else
+ {
+  digitalWrite(ledPin,LOW);
+  }
+}
